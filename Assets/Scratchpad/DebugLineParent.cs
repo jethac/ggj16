e7a5ec54e6ea7c959428f1f90c286d2eed9dd570
Jetha Chan <jethachan@gmail.com>
2016-01-30 17:52:20 +0900

    add a debug line class

---
+++ Assets/Scratchpad/DebugLineParent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DebugLineParent : MonoBehaviour {
+
+    public Color LineColor = Color.red;
+
+    void Start() { }
+    void Update() { }
+
+    void OnDrawGizmos()
+    {
+        Color oldColor = Gizmos.color;
+        Gizmos.color = LineColor;
+        DebugPoint[] points = transform.GetComponentsInChildren<DebugPoint>();
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Gizmos.DrawLine(points[i].transform.position, points[i + 1].transform.position);
+        }
+        Gizmos.color = oldColor;
+    }
+}
