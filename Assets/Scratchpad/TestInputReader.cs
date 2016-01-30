29c5450f0ff7a344132b3125e567a9918a21cdef
Jetha Chan <jethachan@gmail.com>
2016-01-30 13:59:21 +0900

    testinputreader
    visualise the "desired" input direction

---
+++ Assets/Scratchpad/TestInputReader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using InControl;
+
+namespace jethac
+{
+    [RequireComponent(typeof(DesiredHeadingHelper))]
+    public class TestInputReader : MonoBehaviour
+    {
+        Vector3 _mvec_ls, _mvec_dp;
+
+        void FixedUpdate()
+        {
+            var device = InputManager.ActiveDevice;
+            // Support moving with left stick or dpad.
+            _mvec_ls.Set(
+                device.LeftStickX,
+                0.0f,
+                device.LeftStickY
+            );
+            _mvec_dp.Set(
+                device.DPad.X,
+                0.0f,
+                device.DPad.Y
+            );
+
+            DesiredHeadingHelper ddh = gameObject.GetComponent<DesiredHeadingHelper>();
+            ddh.DesiredHeading = (_mvec_ls + _mvec_dp).normalized;
+        }
+    }
+}
