3ecda34512215cff199632c1096bdbab8c5654aa
Jetha Chan <jethachan@gmail.com>
2016-01-30 17:52:06 +0900

    add a debug point class

---
+++ Assets/Scratchpad/DebugPoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DebugPoint : MonoBehaviour {
+
+    public Color MyColor = Color.white;
+
+    void Start() { }
+    void Update() { }
+
+    void OnDrawGizmos()
+    {
+        Color oldColor = Gizmos.color;
+        Gizmos.color = MyColor;
+        Gizmos.DrawSphere(transform.position, 0.05f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + transform.up);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.right);
+
+        Gizmos.color = oldColor;
+    }
+
+}
